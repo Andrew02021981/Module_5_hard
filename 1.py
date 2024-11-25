d5d541b744adf8af52dab056bb2b458bc614a74d
Andrew02021981 <andrejsazonov5487@gmail.com>
2024-11-26 01:18:16 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -5,9 +5,11 @@ class User:
         self.age = int(age)
 
 class Video:
-       def __init__(self, *args, **kwargs):
-        self.title = str
-        self.duration = int
+    # time_now = 0
+    # adult_mode = False
+    def __init__(self, title : str, duration : int, **kwargs):
+        self.title = title
+        self.duration = duration
         self.time_now = 0
         self.adult_mode = False
 
@@ -19,19 +21,28 @@ class UrTube:
         self.current_user = User
 
     def add(self, *args):
-        self.args = args
-        if self.args not in self.videos:
-            self.videos.append(args)
+        for video in args:
+            if video not in self.videos:
+                self.videos.append(video)
+
+    def get_videos(self, search_word: str):
+        for i in range (len(self.videos)):
+            if search_word.lower() in str(self.videos[i]).lower():
+                return self.videos[i]
+                # print(self.videos[i])
 
     def __repr__(self):
-        """Формальное строковое представление."""
-        return f'{self.videos}'
+        return str(self.videos)
 
 ur = UrTube()
 
 v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
-
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode = True)
 ur.add(v1, v2)
 
 print(ur)
+
+# Проверка поиска
+
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
